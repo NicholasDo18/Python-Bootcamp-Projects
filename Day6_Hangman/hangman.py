629bfa661fa9e29b08d93c266feac3a6fe75aa05
NickDo <146508658+NicholasDo18@users.noreply.github.com.>
2025-11-13 20:05:10 -0500

    Implement a feature where we replace blanks for our guesses

--- Day6_Hangman/hangman.py
+++ Day6_Hangman/hangman.py
@@ -1,18 +1,23 @@
 import random
-
 word_list = ["aardvark", "baboon", "camel"]
 
-# TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word. Then print it.
 chosen_word = random.choice(word_list)
 print(chosen_word)
 
-# TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+# TODO-1: Create a "placeholder" with the same number of blanks as the chosen_word
+placeholder = ""
+for character in chosen_word: #adds blank lines for each character for our chosen word
+    placeholder += "_"
+
+print(placeholder)
 guess = input("Guess a letter: ").lower()
 
-# TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word. Print "Right" if it
-#  is, "Wrong" if it's not.
-for letter in chosen_word:
-    if guess == letter:
-        print("Right")
-    else:
-        print("Wrong")
+# TODO-2: Create a "display" that puts the guess letter in the right positions and _ in the rest of the string.
+display = ""
+for letter in chosen_word: #This loops through each letter in the chosen word
+    if guess == letter: #If our guess was correct, it would be added to the display
+        display += letter
+    else: #otherwise, it just adds a blank if we didn't guess right
+        display += "_"
+
+print(display)
